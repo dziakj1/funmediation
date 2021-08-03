416b57671d5eb444273ec0a64d50af9b29ad9610
John J. Dziak <jjd264@psu.edu>
2021-08-03 13:12:18 -0400

    Improve pfr plotting options

--- R/plot_funmediation.R
+++ R/plot_funmediation.R
@@ -12,10 +12,11 @@
 #' @param alpha_level Default is .05 for pointwise 95% confidence intervals.
 #' \describe{
 #' \item{pfr}{For a "pfr" plot, the functional coefficient for predicting the
-#' outcome, Y, from the mediator, M (conditional on X), is shown, by calling
-#' the plot method for the penalized functional regression results. See the
-#' documentation for plot.gam() in the mgcv function for more information,
-#' because the pfr() function uses gam() as a back end.}
+#' outcome, Y, from the mediator, M (conditional on X), is shown.}
+#' \item{pfrgam}{Similar to a "pfr" plot, but uses the plot method for
+#' the penalized functional regression results. See the
+#' documentation for pfr() in the refund library and for
+#' plot.gam() in the mgcv library  for more information.}
 #' \item{coefs}{For a "coefs" plot, the three important functional coefficients
 #' in the model (intercept for predicting M, effect of X on M,
 #' and the effect of M on Y adjusting for X) are plotted one after
@@ -39,6 +40,7 @@
 plot.funmediation <- function(x,
                               use_panes=TRUE,
                               what_plot=c("pfr",
+                                          "pfrgam",
                                           "coefs",
                                           "tvem"),
                               alpha_level=.05,
@@ -47,11 +49,32 @@ plot.funmediation <- function(x,
   if (use_panes==FALSE) {par(mfrow=c(1,1));}
   what <- match.arg(what_plot)
   if (what=="pfr") {
+    par(mfrow=c(1,1),mgp=c(2,1,0));
+    lower <- x$original_results$beta_M_estimate-qnorm(1-alpha_level/2)*x$original_results$beta_M_se;
+    upper <- x$original_results$beta_M_estimate+qnorm(1-alpha_level/2)*x$original_results$beta_M_se;
+    plot(x$original_results$time_grid,
+         x$original_results$beta_M_estimate,
+         xlab=expression(t),
+         ylim=c(min(lower),max(upper)),
+         ylab=expression(beta[M](t)),
+         type="l",
+         tcl=0,
+         lty="solid",
+         lwd=2,
+         mgp=c(1,.1,0),
+         main="Functional Coefficient for\n Mediator Effect on Outcome");
+    lines(x$original_results$time_grid, lower);
+    lines(x$original_results$time_grid, upper);
+    abline(h=0,lty="dotted");
+  }
+  if (what=="pfrgam") {
     if (use_panes) {par(mfrow=c(1,1));}
     plot(x$original_results$funreg_MY_details,
+         se=qnorm(1-alpha_level/2),
+         shade=TRUE,
          xlab="Time",
          ylab="Coefficient function",
-         main="Functional effect of mediator on outcome"); # plot.gam function;
+         main="Functional regression results"); # plot.gam function;
   }
   if (what=="coefs") {
     if (use_panes) {
@@ -67,51 +90,62 @@ plot.funmediation <- function(x,
         stop("There are too many treatment variables to draw this plot legibly.")
       }
     }
+    lower <- x$original_results$alpha_int_estimate-qnorm(1-alpha_level/2)*x$original_results$alpha_int_se;
+    upper <- x$original_results$alpha_int_estimate+qnorm(1-alpha_level/2)*x$original_results$alpha_int_se;
     plot(x$original_results$time_grid,
          x$original_results$alpha_int_estimate,
          xlab=expression(t),
          ylab=expression(alpha[int](t)),
+         ylim=c(min(lower),max(upper)),
+         lwd=2,
+         type="l",
          main="Intercept for Mediator");
-    lines(x$original_results$time_grid,
-          x$original_results$alpha_int_estimate-qnorm(1-alpha_level/2)*x$original_results$alpha_int_se);
-    lines(x$original_results$time_grid,
-          x$original_results$alpha_int_estimate+qnorm(1-alpha_level/2)*x$original_results$alpha_int_se);
+    lines(x$original_results$time_grid, lower);
+    lines(x$original_results$time_grid, upper);
     if (num_treatment_variables==1) {
+      lower <- x$original_results$alpha_X_estimate-qnorm(1-alpha_level/2)*x$original_results$alpha_X_se;
+      upper <- x$original_results$alpha_X_estimate+qnorm(1-alpha_level/2)*x$original_results$alpha_X_se;
       plot(x$original_results$time_grid,
            x$original_results$alpha_X_estimate,
            xlab=expression(t),
            ylab=expression(alpha[X](t)),
+           ylim=c(min(lower),max(upper)),
+           lwd=2,
+           type="l",
            main="Treatment on Mediator");
-      lines(x$original_results$time_grid,
-            x$original_results$alpha_X_estimate-qnorm(1-alpha_level/2)*x$original_results$alpha_X_se);
-      lines(x$original_results$time_grid,
-            x$original_results$alpha_X_estimate+qnorm(1-alpha_level/2)*x$original_results$alpha_X_se);
+      lines(x$original_results$time_grid, lower);
+      lines(x$original_results$time_grid, upper);
     }
     if (num_treatment_variables==2 |
         num_treatment_variables==3 |
         num_treatment_variables==4 ) {
       for (j in 1:num_treatment_variables) {
+        lower <- x$original_results$alpha_X_estimate[[j]]-qnorm(1-alpha_level/2)*x$original_results$alpha_X_se[[j]];
+        upper <- x$original_results$alpha_X_estimate[[j]]+qnorm(1-alpha_level/2)*x$original_results$alpha_X_se[[j]];
         plot(x$original_results$time_grid,
              x$original_results$alpha_X_estimate[[j]],
              xlab=expression(t),
+             ylim=c(min(lower),max(upper)),
              ylab=expression(alpha[X](t)),
+             lwd=2,
+             type="l",
              main=paste(x$important_variable_names$treatment[j],
                         "on Mediator"));
-        lines(x$original_results$time_grid,
-              x$original_results$alpha_X_estimate[[j]]-qnorm(1-alpha_level/2)*x$original_results$alpha_X_se[[j]]);
-        lines(x$original_results$time_grid,
-              x$original_results$alpha_X_estimate[[j]]+qnorm(1-alpha_level/2)*x$original_results$alpha_X_se[[j]]);
+        lines(x$original_results$time_grid, lower);
+        lines(x$original_results$time_grid, upper);
       }
     }
+    lower <- x$original_results$beta_M_estimate-qnorm(1-alpha_level/2)*x$original_results$beta_M_se;
+    upper <- x$original_results$beta_M_estimate+qnorm(1-alpha_level/2)*x$original_results$beta_M_se;
     plot(x$original_results$time_grid,
          x$original_results$beta_M_estimate,
          xlab=expression(t),
+         ylim=c(min(lower),max(upper)),
          ylab=expression(beta[M](t)),
+         lwd=2,
          main="Mediator on Outcome");
-    lines(x$original_results$time_grid,
-          x$original_results$beta_M_estimate-qnorm(1-alpha_level/2)*x$original_results$beta_M_se);
-    lines(x$original_results$time_grid,
-          x$original_results$beta_M_estimate+qnorm(1-alpha_level/2)*x$original_results$beta_M_se);
+    lines(x$original_results$time_grid, lower);
+    lines(x$original_results$time_grid, upper);
     if (num_treatment_variables==1 & use_panes==TRUE) {
       # There is an extra pane, so we can write the indirect effect
       # estimate in the white space.
